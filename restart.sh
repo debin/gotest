16e42b4a1d71e0338510baf6ad559ce61f8c02a1
liangdebin <touch767@gmail.com>
2019-03-31 21:33:15 +0800

    shell脚本

---
+++ restart.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#默认进入的是登录用户的目录
+#cd test/bee
+ps aux |grep hello | grep -v grep | awk '{print $2}' | xargs sudo kill -9
+pidlist=`ps aux |grep hello | grep -v grep | awk '{print $2}'`
+if [ "$pidlist" = "" ]
+then
+    echo "no pid alive!"
+else
+for i in $pidlist
+do
+    echo "Kill the $1 process [ $i ]"
+    sudo kill -9 $i
+done
+fi
+
+sudo nohup ./hello 2>&1 &
+echo "hello start!\n"
+
